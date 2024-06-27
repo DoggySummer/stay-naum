16f3dd016b423ebe08adfb4027915f98ab95f97a
JUNG GIL WOONG <91588779+DoggySummer@users.noreply.github.com>
2024-06-27 10:37:18 +0900

    메인 페이지 사진 추가

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="h-screen w-full">
-      <Image src="/" alt="메인 이미지" />
+      <Image src="/main.webp" alt="메인 이미지" />
     </main>
   )
 }
